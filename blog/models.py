3a54665f6f39fe1522bbc3a8b8262a0add260d77
Leandro Leberon <55678928+leandroleberon@users.noreply.github.com>
2020-03-19 12:57:12 -0400

    mod

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.contrib import messages
+from PIL import Image
 
 
 # Create your models here.
